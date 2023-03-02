44a9a9d5939824af3362480ab28b0d41c20f4f44
Anas Nadeem <anas5678go@gmail.com>
2023-03-02 19:11:42 +0530

    Added existingTabs function

--- popup.js
+++ popup.js
@@ -1,1 +1,29 @@
 const tabBody = document.getElementById('tabBody');
+// const tabs = chrome.tabs.query({
+//   currentWindow: true, 
+// });
+
+const existingTabs = () => {
+	chrome.tabs.query({currentWindow: true}, (tabs) => {
+		for (const tab of tabs) {
+			console.log(tab);
+			displanInTabBody(tab);
+		}
+	});
+}
+existingTabs();
+
+const displanInTabBody = (tab) => {
+	// const pathname = new URL(tab.url);
+	const tabId = tab.id;
+	const tabRow = `<tr>
+		<td id="${tabId}">
+			<a>
+				${tab.title}
+			</a>
+		</td>
+		<td>${tab.active}</td>
+		<td></td>
+	</tr>`
+  	tabBody.innerHTML += tabRow;
+}
