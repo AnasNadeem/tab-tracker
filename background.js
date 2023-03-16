3145bb810af9bf26c434f4af97b66d2255e20416
Anas Nadeem <anas5678go@gmail.com>
2023-03-16 20:53:02 +0530

    Fixed bug

--- background.js
+++ background.js
@@ -25,11 +25,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 			// Update the last tabTracker
 			if(result[tabIdString]['tabTracker'].length > 0){
+				const lastTabIndex = result[tabIdString]['tabTracker'].length - 1;
 				// Skip if the current tab url is same as the last tabTracker url
 				if(result[tabIdString]['tabTracker'][lastTabIndex]['url'] === tab.url){
 					return;
 				}
-				const lastTabIndex = result[tabIdString]['tabTracker'].length - 1;
 				result[tabIdString]['tabTracker'][lastTabIndex]['endTime'] = new Date().getTime();
 				const endTime = result[tabIdString]['tabTracker'][lastTabIndex]['endTime'];
 				const startTime = result[tabIdString]['tabTracker'][lastTabIndex]['startTime'];
